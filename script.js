224a9e1ba7ecca0db47453f617024154520b4a8d
Sean_Dafydd <seandafydd@gmail.com>
2020-06-18 18:01:57 -0700

    navbar align and added welcome message

---
+++ script.js
@@ -0,0 +1,25 @@
+// add listening event for start button
+
+// list of questions and answers object arrays
+
+var questions = [
+    //index 0
+    {
+        question: 'What is a "variable"?',
+        answers: ["a value that changes.", "a box that contains data.", "a string.", "a boolean."],
+        correct: "a box that contains data.",
+    },
+    //index 1 
+    {
+
+    },
+    // index 2
+    // index 3
+    // index 4
+];
+
+//set timer - lookup set interval and close interval
+
+//store remaining time
+
+//loop through questions while documenting to the DOM using ids, use query selector, set up local storage
